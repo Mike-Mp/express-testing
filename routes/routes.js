cf55501ed32cfe625e61e2f66c8f102288f54bc2
Mike-Mp <devmichalis@protonmail.com>
2020-05-05 09:08:19 +0300

    About info and minor styling

--- routes/routes.js
+++ routes/routes.js
@@ -4,4 +4,5 @@ const router = require("express").Router();
 router.get("/", siteController.homePage);
 router.get("/about", siteController.aboutPage);
 router.get("/data", siteController.getData);
+
 module.exports = router;
